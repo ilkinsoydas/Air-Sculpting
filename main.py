61b0e10d449c1483c6d48313c5104fbf09df7643
“ilkinsoydas” <ilkinsoydas123@gmail.com>
2026-09-04 19:04:32 +0300

    feat: connect 4-gesture LSTM model to Blender 3D controls

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import cv2
 import math
-import joblib
 import socket
 import json
-from collections import deque
 from hand_utils import HandProcessor
+import numpy as np
+from tensorflow.keras.models import load_model
 
-model = joblib.load("hand_gesture_model.pkl")
+model = load_model("action.keras")
+actions = np.array(["Extrude", "Rotate", "Idle", "Pinch"])
+
+sequence = []
+threshold = 0.6
+
 processor = HandProcessor()
 
 udp_ip = "127.0.0.1"
@@ -14,7 +19,6 @@ udp_port = 5051
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 cap = cv2.VideoCapture(0)
-prediction_history = deque(maxlen = 5)
 
 
 while True:
@@ -27,26 +31,33 @@ while True:
     frame, keypoints, hand_found, cursor_x, cursor_y = processor.process_frame(frame)
     smooth_prediction = 0
     
-    if hand_found:
-        prediction = model.predict([keypoints])[0]
-        prediction_history.append(prediction)
-        smooth_prediction = 1 if sum(prediction_history) >= 3 else 0
+    if hand_found and keypoints is not None:
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
         
-        if smooth_prediction == 1:
-            cv2.putText(frame, "Pinch", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        else:
-            cv2.putText(frame, "Open Hand", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        
-        data_packet = {
-            "gesture": int(smooth_prediction),
-            "x": float(cursor_x),
-            "y": float(cursor_y)
-        }
-
-        try:
-            sock.sendto(json.dumps(data_packet).encode("utf-8"), (udp_ip, udp_port))
-        except Exception:
-            pass
+        if len(sequence) == 30:
+            input_data = np.expand_dims(sequence, axis = 0)
+            res = model(input_data, training = False)[0].numpy()
+            
+            if res[np.argmax(res)] > threshold:
+                action_name = actions[np.argmax(res)]
+                confidence = res[np.argmax(res)] * 100
+                
+                text = f"{action_name}:  ({confidence: .1f}%)"
+                cv2.putText(frame, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                
+                data_packet = {
+                    "gesture": action_name,
+                    "x": float(cursor_x),
+                    "y": float(cursor_y)
+                }
+                
+                try:
+                    sock.sendto(json.dumps(data_packet).encode("utf-8"), (udp_ip, udp_port))
+                except Exception:
+                    pass
+    else:
+        sequence = []
 
     cv2.imshow("Test camera", frame)
     
